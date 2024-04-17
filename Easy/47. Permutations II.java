6a809f3906fbe37ff1c35a83d4626ab0cd3ae890
Jianuo-Always-Coding <115911379+Jianuo-Always-Coding@users.noreply.github.com>
2024-04-17 18:38:02 -0400

    up

---
+++ Easy/47. Permutations II.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        helper(nums, 0, result);
+        return result;
+    }
+
+    private void helper(int[] nums, int index, List<List<Integer>> result) {
+        // base case
+        if (index == nums.length) {
+            List<Integer> list = new ArrayList<>();
+            for (Integer i : nums) {
+                list.add(i);
+            }
+            result.add(list);
+            return;
+        }
+        Set<Integer> set = new HashSet<>();
+        for (int i = index; i < nums.length; i++) {
+            if (!set.contains(nums[i])) {
+                set.add(nums[i]);
+                swap(nums, index, i);
+                ;
+                helper(nums, index + 1, result);
+                swap(nums, index, i);
+            }
+        }
+    }
+
+    private void swap(int[] array, int left, int right) {
+        int temp = array[left];
+        array[left] = array[right];
+        array[right] = temp;
+    }
+}
