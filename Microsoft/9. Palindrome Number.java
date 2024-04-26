6aca3eb12d575b97cc1092d7ff0423b6392ffc80
Jianuo-Always-Coding <115911379+Jianuo-Always-Coding@users.noreply.github.com>
2024-04-26 11:33:45 -0400

    Create 9. Palindrome Number.java

---
+++ Microsoft/9. Palindrome Number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        int sum = x;
+        int val = 0;
+        while (x != 0) {
+            val = val * 10 + x % 10;
+            x = x / 10;
+        }
+        if (val == sum) {
+            return true;
+        }
+        return false;
+    }
+}
