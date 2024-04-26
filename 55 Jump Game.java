f9707cbcb3d8e58d61ee218369b24e6db652ea38
Jianuo-Always-Coding <115911379+Jianuo-Always-Coding@users.noreply.github.com>
2024-04-26 16:33:38 -0400

    Update 55 Jump Game.java

--- 55 Jump Game.java
+++ 55 Jump Game.java
@@ -4,15 +4,18 @@ class Solution {
         if (nums == null || nums.length <= 1) {
             return true;
         }
+        // 能否跳到
         boolean[] canArrive = new boolean[nums.length];
         int n = nums.length;
         canArrive[0] = true;
 
         for (int i = 0; i < n; i++) {
             if (canArrive[i]) {
+                // 是否跳超过
                 if (i + nums[i] >= n - 1) {
                     return true;
                 }
+                // 把能到达的位置都变为true
                 for (int j = 1; j <= nums[i]; j++) {
                     canArrive[i + j] = true;
                 }
