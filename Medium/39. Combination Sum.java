4cac8395dd058db873896f62d8204457c3d88a7b
Jianuo-Always-Coding <115911379+Jianuo-Always-Coding@users.noreply.github.com>
2024-04-27 09:50:47 -0400

    Update 39. Combination Sum.java

--- Medium/39. Combination Sum.java
+++ Medium/39. Combination Sum.java
@@ -8,22 +8,24 @@ class Solution {
 
 
     private void helper(List<List<Integer>> result, List<Integer> layer, int[] candidates, int index, int rest) {
-        // base case
+        // base case 最后一个数能不能被全部放完，可以为0
         if (index == candidates.length - 1 && rest % candidates[candidates.length - 1] == 0) {
             int count = rest / candidates[candidates.length - 1];
             for (int i = 0; i < count; i++) {
                 layer.add(candidates[candidates.length - 1]);
             }
             result.add(new ArrayList<>(layer));
+            //放完要删除和return
             for (int i = 0; i < count; i++) {
                 layer.remove(layer.size() - 1);
             }
             return ;
+        // 不放完也得return
         } else if (index == candidates.length - 1) {
             return ;
         }
 
-
+        // 每一层能放多少
         int count = rest / candidates[index];
         helper(result, layer, candidates, index + 1, rest);
         for (int i = 1; i <= count; i++) {
@@ -33,8 +35,5 @@ class Solution {
         for (int i = 0; i < count; i++) {
             layer.remove(layer.size() - 1);
         }
-
-
-
     }
 }
