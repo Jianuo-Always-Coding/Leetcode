3efce2d1f1dca6b901a6e9b7cf742c93b4f11239
Jianuo-Always-Coding <115911379+Jianuo-Always-Coding@users.noreply.github.com>
2024-04-27 11:16:35 -0400

    Update 253. Meeting Rooms II.java

--- Medium/253. Meeting Rooms II.java
+++ Medium/253. Meeting Rooms II.java
@@ -6,14 +6,17 @@ class Solution {
             starts[i] = intervals[i][0];
             ends[i] = intervals[i][1];
         }
-        Arrays.sort(starts);
-        Arrays.sort(ends);
+        Arrays.sort(starts); // 所有会议的开始时间
+        Arrays.sort(ends); // 所有会议的结束时间
         int rooms = 0;
         int endPoint = 0;
         for(int i=0; i<starts.length; i++) {
-            if(starts[i]<ends[endPoint])
+            if(starts[i] < ends[endPoint]) 
+                // 会议没结束但是有新会议，增加一个room
+                // 因为增加了room，所以结束时间不用动
                 rooms++;
             else
+                //没有冲突，结束时间需要增加
                 endPoint++;
         }
         return rooms;
