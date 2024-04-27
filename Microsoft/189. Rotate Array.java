06005bf54fa140e900187fd57fd26f8201247823
Jianuo-Always-Coding <115911379+Jianuo-Always-Coding@users.noreply.github.com>
2024-04-27 10:39:35 -0400

    Create 189. Rotate Array.java

---
+++ Microsoft/189. Rotate Array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+
+        rotate(nums, 0, nums.length-1);
+        rotate(nums, 0, k-1);
+        rotate(nums, k, nums.length-1);
+    }
+
+    private void rotate(int[] nums, int i, int j) {
+        while (i < j) {
+            int tmp = nums[i];
+            nums[i++] = nums[j];
+            nums[j--] = tmp;
+        }
+    }
+}
