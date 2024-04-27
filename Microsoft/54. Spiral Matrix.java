6110a86d01304009f05304f7879ba07b35c99adc
Jianuo-Always-Coding <115911379+Jianuo-Always-Coding@users.noreply.github.com>
2024-04-26 20:31:54 -0400

    Update 54. Spiral Matrix.java

--- Microsoft/54. Spiral Matrix.java
+++ Microsoft/54. Spiral Matrix.java
@@ -1,48 +1,54 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        // control four variable left, right, up and down, for loop to print
-        // if M != N, need to deal with corner case
         List<Integer> result = new ArrayList<>();
-        if (matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0) {
-            return result;
-        }
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        int up = 0;
-        int left = 0;
-        int right = cols - 1;
-        int down = rows - 1;
-        while (left < right && up < down) {
-            for (int i = left; i <= right; i++) {
-                result.add(matrix[up][i]);
-            }
-            for (int i = up + 1; i <= down - 1; i++) {
-                result.add(matrix[i][right]);
-            }
-            for (int i = right; i >= left; i--) {
-                result.add(matrix[down][i]);
-            }
-            for (int i = down - 1; i >= up + 1; i--) {
-                result.add(matrix[i][left]);
-            }
-            left++;
-            right--;
-            up++;
-            down--;
-        }
-        if (left > right || up > down) {
-            return result;
-        }
-        // only have one col
-        if (left == right) {
-            for (int i = up; i <= down; i++) {
-                result.add(matrix[i][left]);
-            }
-        } else {
-            for (int i = left; i <= right; i++) {
-                result.add(matrix[up][i]);
-            }
-        }
+        printNode(matrix, 0, 0, matrix.length - 1, matrix[0].length - 1, result);
         return result;
+    }
+
+    private void printNode(int[][] matrix, int r1, int c1, int r2, int c2, List<Integer> result) {
+
+        if (r1 > r2 || c1 > c2) {
+            return;
+        }
+        // base case 长宽谁大不一定
+        while (r1 <= r2 && c1 <= c2) {
+            // 只有一行
+            if (r1 == r2) {
+                for (int c = c1; c <= c2; c++) {
+                    result.add(matrix[r1][c]);
+
+                }
+                return;
+            }
+
+            // 只有一列
+            if (c1 == c2) {
+                for (int r = r1; r <= r2; r++) {
+                    result.add(matrix[r][c1]);
+                }
+                return;
+            }
+
+            // 旋转打印
+            for (int c = c1; c < c2; c++) {
+                result.add(matrix[r1][c]);
+            }
+
+            for (int r = r1; r < r2; r++) {
+                result.add(matrix[r][c2]);
+            }
+
+            for (int c = c2; c > c1; c--) {
+                result.add(matrix[r2][c]);
+            }
+
+            for (int r = r2; r > r1; r--) {
+                result.add(matrix[r][c1]);
+            }
+            r1++;
+            r2--;
+            c1++;
+            c2--;
+        }
     }
 }
