0d552002a37a604af4504967786ab379ff515cba
Jianuo-Always-Coding <115911379+Jianuo-Always-Coding@users.noreply.github.com>
2024-04-26 23:08:31 -0400

    Update 97 Interleaving String.java

--- Nvidia/97 Interleaving String.java
+++ Nvidia/97 Interleaving String.java
@@ -6,7 +6,7 @@ class Solution {
             // if dp[i][j] == true: 
             //     if (s1.charAt(i) == s3.charAt(i + j)) put into queue
             //     if (s2.charAt(i) == s3.charAt(i + j)) put into queue
-
+        // 用一个Queue 模仿dfs，每次加一个，加什么
         // corner case
         if (s1.length() + s2.length() != s3.length()) {
             return false;
@@ -29,12 +29,16 @@ class Solution {
             int[] pos = q.poll();
             int i = pos[0];
             int j = pos[1];
+            // 用完了
             if (i == s1.length() && j == s2.length()) {
                 return true;
             }
+            // 每次加一个，看一下哪面能加
+            // 第一个还能加，加第一个
             if (i < s1.length() && s1.charAt(i) == s3.charAt(i + j)) {
                 q.offer(new int[] {i + 1, j});
             }
+            // 第二个还能加，加第二个
             if (j < s2.length() && s2.charAt(j) == s3.charAt(i + j)) {
                 q.offer(new int[] {i, j + 1});
             }
