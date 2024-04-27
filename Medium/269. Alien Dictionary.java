f23096024006e6e7774fa3ca5b5c5f92245ce972
Jianuo-Always-Coding <115911379+Jianuo-Always-Coding@users.noreply.github.com>
2024-04-27 18:59:14 -0400

    Update 269. Alien Dictionary.java

--- Medium/269. Alien Dictionary.java
+++ Medium/269. Alien Dictionary.java
@@ -5,12 +5,12 @@ class Solution {
             return "";
         }
 
-        Map<Character, Set<Character>> map = new HashMap<>(); 
+        Map<Character, Set<Character>> map = new HashMap<>(); 在char后面的字母都有什么
         int[] degree = new int[26];
         for (int i = 0; i < 26; i++) {
             degree[i] = -1;
         }
-        int count = 0;
+        int count = 0; // 记录一共有多少个字母
 
         // 先找出所有的字母，然后将他们的入度设为0
         for (String word : words) {
@@ -32,17 +32,21 @@ class Solution {
             int b = 0;
             String word1 = words[i - 1];
             String word2 = words[i];
+            // 不合法，前面一样，第二个单词长度短
             if (word1.startsWith(word2) && word1.length() > word2.length()) {
                 return "";
             }
             while (a < word1.length() && b < word2.length()) {
+                // 去除一样的字母，找到第一个奴一样的字母
                 if (word1.charAt(a) == word2.charAt(b)) {
                     a++;
                     b++;
                 } else {
+                    // 加入list，并且将后面的字母入度加一
                     if (map.get(word1.charAt(a)).add(word2.charAt(b))) {
                         degree[word2.charAt(b) - 'a']++;
                     }
+                    //找到了找下一对
                     break;
                 }
             }
@@ -51,6 +55,7 @@ class Solution {
         Queue<Character> q = new ArrayDeque<>();
         StringBuilder sb = new StringBuilder();
 
+        // 找到所有存在且入度为0的字母
         for (int i = 0; i < 26; i++) {
             if (degree[i] == 0) {
                 q.offer((char)(i + 'a'));
@@ -68,9 +73,11 @@ class Solution {
             }
         }
 
+        // 所有字母都找到了
         if (sb.length() == count) {
             return sb.toString();
         }
+        // 没找到所有字母
         return "";
         
     }
