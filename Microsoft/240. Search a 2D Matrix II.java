7c11f2f5695b65760d0134e4dd07c18de5feeb41
Jianuo-Always-Coding <115911379+Jianuo-Always-Coding@users.noreply.github.com>
2024-04-28 16:27:03 -0400

    Create 240. Search a 2D Matrix II.java

---
+++ Microsoft/240. Search a 2D Matrix II.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix == null || matrix.length < 1 || matrix[0].length <1) {
+            return false;
+        }
+        // 往下走一定增加，往左走减小，最终找到或者超出界限
+        // 也可以使用左下角的点
+        int col = matrix[0].length-1;
+        int row = 0;
+        while(col >= 0 && row <= matrix.length-1) {
+            if(target == matrix[row][col]) {
+                return true;
+            } else if(target < matrix[row][col]) {
+                col--;
+            } else if(target > matrix[row][col]) {
+                row++;
+            }
+        }
+        return false;
+    }
+}
