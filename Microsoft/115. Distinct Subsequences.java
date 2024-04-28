2f68677e4ac5b5e1d5ca9ef3366f56c47626a5aa
Jianuo-Always-Coding <115911379+Jianuo-Always-Coding@users.noreply.github.com>
2024-04-28 12:52:28 -0400

    Create 115. Distinct Subsequences.java

---
+++ Microsoft/115. Distinct Subsequences.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int numDistinct(String s, String t) {
+        int m = s.length(), n = t.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        // Initialize dp array
+        // t为空字符的话 s至少有一个匹配
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                // 最后一个字母派不上用场
+                dp[i][j] = dp[i - 1][j];
+                // 最后一个字母能派上用场
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] += dp[i - 1][j - 1];
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
