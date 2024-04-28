0240e026c3c5a2b4a27c20997788310a6d17231b
Jianuo-Always-Coding <115911379+Jianuo-Always-Coding@users.noreply.github.com>
2024-04-27 23:41:53 -0400

    Create 426. Convert Binary Search Tree to Sorted Doubly Linked List.java

---
+++ Microsoft/426. Convert Binary Search Tree to Sorted Doubly Linked List.java
@@ -0,0 +1,47 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val,Node _left,Node _right) {
+        val = _val;
+        left = _left;
+        right = _right;
+    }
+};
+*/
+
+class Solution {
+    Node head;
+    Node tail;
+    public Node treeToDoublyList(Node root) {
+        if (root == null) return null;
+        dfs(root);
+        head.left = tail;
+        tail.right = head;
+        return head;
+    }
+    public void dfs(Node root) {
+        if (root == null) return;
+        dfs(root.left);
+        if (head == null) {
+            head = root;
+        }
+        if (tail == null) {
+            tail = root;
+        } else {
+            root.left = tail;
+            tail.right = root;
+            tail = root;
+        }
+        dfs(root.right);
+    }
+}
