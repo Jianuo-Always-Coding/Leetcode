03241edb1aa2a646341c813ce5def984c829ab81
Jianuo-Always-Coding <115911379+Jianuo-Always-Coding@users.noreply.github.com>
2024-05-06 16:47:52 -0400

    Update 31. Next Permutation.java

--- Tiktok/31. Next Permutation.java
+++ Tiktok/31. Next Permutation.java
@@ -1,44 +1,52 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int i = nums.length - 2;
-        
-
-        // 从右面数，找到第一个不是降序的数字的index
-        while (i >= 0 && nums[i] >= nums[i + 1]) {
-            i--;
-        }
-        // 如果没找到说明是最大的，reverse就可以
-        if (i == -1) {
-            reverse(nums, 0);
+        // corner case
+        if (nums == null || nums.length <= 1) {
             return ;
         }
-        // 找到了, 找最靠近右面的一个比他大的数交换
-        if (i >= 0) {
-            // Find the largest index（最靠右面） j greater than i such that nums[i] < nums[j]
-            int j = nums.length - 1;
-            while (j >= i && nums[i] >= nums[j]) {
-                j--;
+
+        // 找到从右面数第一个小于前一个的数的位置（大于等于都不行）
+        int index1 = nums.length - 2;
+        while (index1 >= 0) {
+            if (nums[index1] >= nums[index1 + 1]) {
+                index1--;
+            } else {
+                break;
             }
-            // Swap nums[i] and nums[j]
-            swap(nums, i, j);
         }
-        // Reverse the sub-array nums[i + 1:]， 从最小升序开始
-        reverse(nums, i + 1);
-    }
-    
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-    
-    private void reverse(int[] nums, int start) {
-        int i = start, j = nums.length - 1;
-        while (i < j) {
-            swap(nums, i, j);
-            i++;
-            j--;
+
+        // 全部降序
+        if (index1 == -1) {
+            reverse(nums, 0, nums.length - 1);
+            return ;
         }
+
+        // 找到最靠右面第一个比index大的数
+        int index2 = nums.length - 1;
+        while (index2 >= 0) {
+            if (nums[index2] <= nums[index1]) {
+                index2--;
+            } else {
+                break;
+            }
+        }
+        if (index2 == -1) {
+            return ;
+        }
+        swap(nums, index1, index2);
+        reverse(nums, index1 + 1, nums.length - 1);
+        return ;
+    }
+
+    private void reverse(int[] array, int start, int end) {
+        while (start  < end) {
+            swap(array, start++, end--);
+        }
+    }
+
+    private void swap(int[] array, int a, int b) {
+        int temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
     }
 }
-
