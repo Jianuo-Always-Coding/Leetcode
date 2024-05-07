3a5eab83cf8cb5162e6b058d2ea9cfd4a9246e15
Jianuo-Always-Coding <115911379+Jianuo-Always-Coding@users.noreply.github.com>
2024-05-06 22:52:05 -0400

    Update 57. Insert Interval.java

--- Medium/57. Insert Interval.java
+++ Medium/57. Insert Interval.java
@@ -4,18 +4,20 @@ class Solution {
         
         int n = intervals.length;
         int index = 0;
-
+        // 结束在新interval开始之前，可以放心加
         while (index < n && intervals[index][1] < newInterval[0]) {
             merge.add(intervals[index]);
             index++;
         }
-
+        // 一直加到最后，或者到newInterval结束时间 小于下一个interval开始时间结束
         while (index < n && intervals[index][0] <= newInterval[1]) {
             newInterval[0] = Math.min(newInterval[0], intervals[index][0]);
             newInterval[1] = Math.max(newInterval[1], intervals[index][1]);
             index++;
         }
         merge.add(newInterval);
+
+        // 加入剩下的
         while (index < n) {
             merge.add(intervals[index]);
             index++;
