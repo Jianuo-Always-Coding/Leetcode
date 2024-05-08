4b85552592745eba0f7f2ee047708946d4641c37
Jianuo-Always-Coding <115911379+Jianuo-Always-Coding@users.noreply.github.com>
2024-05-07 23:07:33 -0400

    Create 76. Minimum Window Substring.java

---
+++ Tiktok/76. Minimum Window Substring.java
@@ -0,0 +1,53 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        if (s == null || s.length() < t.length()) {
+            return "";
+        }
+        
+        Map<Character, Integer> map = new HashMap<>();
+        char c = ' ';
+
+        // 初始化map 
+        for (int i = 0 ; i < t.length(); i++) {
+            c = t.charAt(i);
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c) + 1);
+            } else {
+                map.put(c, 1);
+            }
+        }
+        // 需要匹配的字母数量
+        int matchedNeed = map.size();
+
+        String result = "";
+        int start = 0;
+        int end = 0;
+        // 开始寻找
+        while (end < s.length()) {
+            while (end < s.length() && matchedNeed > 0) {
+                c = s.charAt(end++);
+                if (map.containsKey(c)) {
+                    map.put(c, map.get(c) - 1);
+                    if (map.get(c) == 0) {
+                        matchedNeed--;
+                    }
+                }
+            }
+
+            while (start < s.length() && matchedNeed == 0) {
+                c = s.charAt(start++);
+                if (map.containsKey(c)) {
+                    map.put(c, map.get(c) + 1);
+                    if (map.get(c) == 1) {
+                        if (result == "" || end - 1 - (start - 1) + 1 < result.length()) {
+                            result = s.substring(start - 1, end);
+                        } 
+                        matchedNeed++;
+                    }
+                }
+            }
+
+        }
+        return result;
+    }
+}
