ca5eb67694272e439997f5e56a31871cb52fa0c4
Jianuo-Always-Coding <115911379+Jianuo-Always-Coding@users.noreply.github.com>
2024-05-12 22:48:55 -0400

    Update 1405. Longest Happy String.java

--- Tiktok/1405. Longest Happy String.java
+++ Tiktok/1405. Longest Happy String.java
@@ -1,29 +1,29 @@
 class Solution {
     public String longestDiverseString(int a, int b, int c) {
-        StringBuilder sb = new StringBuilder();
-        
-        int totalLength = a + b + c;
-        int continuousA = 0, continuousB = 0, continuousC = 0;
-        
-        for(int i = 0; i < totalLength; i++) {
-            if ((a >= b && a >= c && continuousA != 2) || (continuousB == 2 && a > 0) || (continuousC == 2 && a > 0))  {
+        StringBuilder sb=new StringBuilder();
+
+        int totalLength=a+b+c;
+        int continuousA=0,continuousB=0,continuousC=0;
+        for(int i=0;i<totalLength;i++){
+            // 先判断是不是最多的
+            if(a > 0 && ((a>=b && a>=c && continuousA!=2)||(continuousB==2 && a>=c) ||(continuousC==2 && a>=b))){
                 sb.append("a");
                 a--;
                 continuousA++;
-                continuousB = 0;
-                continuousC = 0;  
-            } else if ((b >= a && b >= c && continuousB != 2) || (continuousA == 2 && b > 0) || (continuousC == 2 && b > 0)) {
+                continuousB=0;
+                continuousC=0;
+            }else if(b > 0 && ((b>=a && b>=c && continuousB!=2)||(continuousA==2 && b>=c) ||(continuousC==2 && b>=a))){
                 sb.append("b");
                 b--;
                 continuousB++;
-                continuousA = 0;
-                continuousC = 0;
-            } else if ((c >= a && c >= b && continuousC != 2) || (continuousB == 2 && c > 0) || (continuousA == 2 && c > 0)) {
+                continuousA=0;
+                continuousC=0;
+            }else if(c > 0 && ((c>=b && c>=a && continuousC!=2)||(continuousB==2 && c>=a) ||(continuousA==2 && c>=b))){
                 sb.append("c");
                 c--;
                 continuousC++;
-                continuousA = 0;
-                continuousB = 0;  
+                continuousB=0;
+                continuousA=0;
             }
         }
         return sb.toString();
