b6c98eb7a4dba0699c4734228b7bd35e23167726
Jianuo-Always-Coding <115911379+Jianuo-Always-Coding@users.noreply.github.com>
2024-05-30 20:21:16 -0400

    Update 1062. Longest Repeating Substring.java

--- Tiktok/1062. Longest Repeating Substring.java
+++ Tiktok/1062. Longest Repeating Substring.java
@@ -4,7 +4,7 @@ class Solution {
         // 表示的是字符串的长度
         while(left<right){
             int mid = left + (right - left) / 2;
-            if(search(mid, s)) left = mid + 1;
+            if(search(mid, s)) left = mid + 1; // 有重复
             else right = mid;
         }
         return left-1;
@@ -12,10 +12,11 @@ class Solution {
     private boolean search(int len, String s){
         // 从0开始长度为len的结果放入set中
         Set<Integer> set = new HashSet<>();
+        // 从index为0开始便利
         for(int i = 0; i <= s.length()-len; i++){
             String key = s.substring(i, i+len);
             int code = key.hashCode();
-            if(!set.add(code)) return true;
+            if(!set.add(code)) return true; // 找到重复
         }
         return false;
     }
